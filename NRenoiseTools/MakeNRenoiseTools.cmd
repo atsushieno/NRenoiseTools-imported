754ace064b8d806fbc3948a487e583127baa9f99
alexandre_mutel <none@none.com>
2008-09-21 18:33:23 +0000

    - Clean up depdencies - Add Make for Windows Installer - Add Make for Chm

--- NRenoiseTools/MakeNRenoiseTools.cmd
+++ NRenoiseTools/MakeNRenoiseTools.cmd
@@ -1,6 +1,7 @@
 setlocal
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+set DXROOT=C:\Program Files\Sandcastle
 set PATH=..\nant\bin;%PATH%
 REM msbuild /t:Build /p:Configuration=Release NetAsm.sln
-nant
+nant %1 %2 %3 %4 %5 %6
 pause
